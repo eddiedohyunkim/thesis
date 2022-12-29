ab99c4fa61b3d400fd6c2a20ca81670d73e0bca0
Eddie Dohyun Kim <eddiekimdohyun@gmail.com>
2022-12-30 00:58:27 +0900

    first push

---
+++ camera/script.js
@@ -0,0 +1,82 @@
+const typeArea = document.getElementById('typeArea');
+
+const sentence = 'wa@';
+
+for(let element of sentence){
+	if(element=='@'){ element='at'};
+	createLetter(element);	
+}
+
+function createLetter(letter){
+	const letterCont = document.createElement('div');
+	letterCont.className = 'letterCont';
+	typeArea.appendChild(letterCont);
+
+	const widthSetImg = document.createElement('img');
+	widthSetImg.className = 'width-set-img';
+	widthSetImg.src = `/assets/${letter}.svg`;
+	letterCont.appendChild(widthSetImg);
+
+	const video1 = document.createElement('video');
+	video1.className = 'video1';
+	video1.autoplay = true;
+	letterCont.appendChild(video1);	
+
+	const maskCont = document.createElement('div');
+	maskCont.className = 'maskCont';
+	letterCont.appendChild(maskCont);
+
+	const mask = document.createElement('div');
+	mask.className = 'mask';
+	mask.style.webkitMaskImage = `url(/assets/${letter}.svg)`;
+	mask.style.maskImage = `url(/assets/${letter}.svg)`;
+	maskCont.appendChild(mask);		
+
+	const video2 = document.createElement('video');
+	video2.className = 'video2';
+	video2.autoplay = true;
+	mask.appendChild(video2);	
+}
+
+
+
+
+const constraints = {
+	audio: false,
+	video: { facingMode: "user", frameRate: { min: 20, ideal: 30, max: 60 }}
+};
+
+navigator.mediaDevices.getUserMedia(constraints)
+  .then((mediaStream) => {
+    const video = document.querySelectorAll('video');
+    for(let elements of video){
+    	elements.srcObject = mediaStream;
+    	elements.onloadedmetadata = () => {
+      		elements.play();
+    	};
+    }
+  })
+  .catch((err) => {
+    // always check for errors at the end.
+    console.error(`${err.name}: ${err.message}`);
+  });
+
+const constraints2 = {
+	audio: false,
+	video: { facingMode: "user", frameRate: { min: 3, ideal: 3, max: 3 }}
+};
+
+navigator.mediaDevices.getUserMedia(constraints2)
+  .then((mediaStream) => {
+    const video2 = document.querySelectorAll('.video2');
+    for(let elements of video2){
+    	elements.srcObject = mediaStream;
+    	elements.onloadedmetadata = () => {
+      		elements.play();
+    	};
+    }
+  })
+  .catch((err) => {
+    // always check for errors at the end.
+    console.error(`${err.name}: ${err.message}`);
+  });
