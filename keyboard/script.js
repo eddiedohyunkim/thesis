2eac1a99dcef3df86a97a8472cf4b4d364274622
Eddie Dohyun Kim <eddiekimdohyun@gmail.com>
2023-01-02 04:23:43 +0900

    first push

---
+++ keyboard/script.js
@@ -0,0 +1,14 @@
+const mobileTyped = document.getElementById('mobile-typed');
+const logCont = document.getElementById('logCont');
+
+mobileTyped.addEventListener('keyup', function(event) {
+	const input = this.value;
+	console.log(input);
+	// if( event.key.length == 1 ){
+		const log = document.createElement('p');
+		log.className = 'typed';
+		log.innerHTML = input;
+		logCont.appendChild(log);
+	// }
+	window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);	
+}, true);
