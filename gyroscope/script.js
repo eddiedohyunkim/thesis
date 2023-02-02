f83101a054db4cac9cd73e8f6e1b47598b5dd0a2
Eddie Dohyun Kim <eddiekimdohyun@gmail.com>
2023-02-01 19:55:05 -0500

    clean up base code

--- gyroscope/script.js
+++ gyroscope/script.js
@@ -1,19 +1,3 @@
-// function success(position) {
-//   console.log(position.coords.latitude, position.coords.longitude);
-// }
-
-// function error() {
-//   alert('Sorry, no position available.');
-// }
-
-// const options = {
-//   enableHighAccuracy: true,
-//   maximumAge: 0,
-//   timeout: 1000
-// };
-
-// const watchID = navigator.geolocation.watchPosition(success, error, options);
-
 function handleOrientation(event) {
   updateFieldIfNotNull('orientation-a', event.alpha);
   updateFieldIfNotNull('orientation-b', event.beta);
@@ -32,34 +16,6 @@ function updateFieldIfNotNull(fieldName, value, precision=0){
     document.getElementById(fieldName).innerHTML = value.toFixed(precision);
 }
 
-// let is_running = false;
-// let demo_button = document.getElementById("start_demo");
-// demo_button.onclick = function(e) {
-//   e.preventDefault();
-  
-//   // Request permission for iOS 13+ devices
-//   if (
-//     DeviceMotionEvent &&
-//     typeof DeviceMotionEvent.requestPermission === "function"
-//   ) {
-//     DeviceMotionEvent.requestPermission();
-//   }
-  
-//   if (is_running){
-//     window.removeEventListener("deviceorientation", handleOrientation);
-//     demo_button.innerHTML = "Start";
-//     demo_button.classList.add('btn-success');
-//     demo_button.classList.remove('btn-danger');
-//     is_running = false;
-//   }else{
-//     window.addEventListener("deviceorientation", handleOrientation);
-//     document.getElementById("start_demo").innerHTML = "Stop";
-//     demo_button.classList.remove('btn-success');
-//     demo_button.classList.add('btn-danger');
-//     is_running = true;
-//   }
-// };
-
 let is_running = false;
 let holdArea = document.getElementById('holdArea');
 holdArea.addEventListener('touchstart', (event) => {
@@ -74,8 +30,6 @@ holdArea.addEventListener('touchend', (event) => {
   run();
 });
 
-
-
 function run(){
   if (
     DeviceMotionEvent &&
@@ -85,42 +39,10 @@ function run(){
   }
   
   if (is_running){
-    // setInterval( function(){console.log(true)}, 100)
-    
-    // window.removeEventListener("devicemotion", handleMotion);
     window.removeEventListener("deviceorientation", handleOrientation);
-    holdArea.classList.add('btn-success');
-    holdArea.classList.remove('btn-danger');
-    // demo_button.innerHTML = "Start";
-    // demo_button.classList.add('btn-success');
-    // demo_button.classList.remove('btn-danger');
     is_running = false;
   }else{
-    // console.log(false)
-    // window.addEventListener("devicemotion", handleMotion);
     window.addEventListener("deviceorientation", handleOrientation);
-    // document.getElementById("start_demo").innerHTML = "Stop";
-    holdArea.classList.remove('btn-success');
-    holdArea.classList.add('btn-danger');
     is_running = true;
   }
 }
-
-/*
-Light and proximity are not supported anymore by mainstream browsers.
-window.addEventListener('devicelight', function(e) {
-   document.getElementById("DeviceLight").innerHTML="AmbientLight current Value: "+e.value+" Max: "+e.max+" Min: "+e.min;
-});
-
-window.addEventListener('lightlevel', function(e) {
-   document.getElementById("Lightlevel").innerHTML="Light level: "+e.value;
-});
-
-window.addEventListener('deviceproximity', function(e) {
-   document.getElementById("DeviceProximity").innerHTML="DeviceProximity current Value: "+e.value+" Max: "+e.max+" Min: "+e.min;
-});
-
-window.addEventListener('userproximity', function(event) {
-   document.getElementById("UserProximity").innerHTML="UserProximity: "+event.near;
-});
-*/
