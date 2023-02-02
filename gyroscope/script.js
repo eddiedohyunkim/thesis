a3269505b33afcf5a13e3e12d106fcca58862586
Eddie Dohyun Kim <eddiekimdohyun@gmail.com>
2023-02-01 19:26:41 -0500

    clean up base code

---
+++ gyroscope/script.js
@@ -0,0 +1,27 @@
+function handleOrientation(event) {
+  updateFieldIfNotNull('orientation-a', event.alpha);
+  updateFieldIfNotNull('orientation-b', event.beta);
+  updateFieldIfNotNull('orientation-g', event.gamma);
+  incrementEventCount();
+}
+
+function incrementEventCount(){
+  let counterElement = document.getElementById("num-observed-events")
+  let eventCount = parseInt(counterElement.innerHTML)
+  counterElement.innerHTML = eventCount + 1;
+}
+
+function updateFieldIfNotNull(fieldName, value, precision=0){
+  if (value != null)
+    document.getElementById(fieldName).innerHTML = value.toFixed(precision);
+}
+
+function run(){
+  if (
+    DeviceMotionEvent &&
+    typeof DeviceMotionEvent.requestPermission === "function"
+  ) {
+    DeviceMotionEvent.requestPermission();
+  }
+  window.removeEventListener("deviceorientation", handleOrientation);
+}
