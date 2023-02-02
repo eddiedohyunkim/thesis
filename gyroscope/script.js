ef50c39418ac8c83fd82fd57f4762f0836436fbc
Eddie Dohyun Kim <eddiekimdohyun@gmail.com>
2023-02-01 23:25:20 -0500

    detect upside down 2nd attempt

--- gyroscope/script.js
+++ gyroscope/script.js
@@ -11,7 +11,9 @@ function handleOrientation(event) {
 
 function detectUpsideDown(x, y, z){
   if((x > 170 && x <= 180) || (x >= -180 && x < -170)){
-    document.body.style.backgroundColor = 'red';
+    if(y >= -10 && y <= 10){
+      document.body.style.backgroundColor = 'red';
+    }
   }else{
     document.body.style.backgroundColor = 'white';
   }
